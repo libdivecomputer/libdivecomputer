4ec390303ee941edb7118f92e4b3c4f71c70785f
Jef Driesen <jefdriesen@users.sourceforge.net>
2008-07-07 12:54:25 +0000

    Remove all remaining pieces of the legacy api from the Reefnet code.

--- src/reefnet.h
+++ src/reefnet.h
@@ -1,15 +1,6 @@
 #ifndef REEFNET_H
 #define REEFNET_H
 
-#include "device.h"
-
-#define REEFNET_SUCCESS         0
-#define REEFNET_ERROR          -1
-#define REEFNET_ERROR_IO       -2
-#define REEFNET_ERROR_MEMORY   -3
-#define REEFNET_ERROR_PROTOCOL -4
-#define REEFNET_ERROR_TIMEOUT  -5
-
 #include "reefnet_sensuspro.h"
 #include "reefnet_sensusultra.h"
 
