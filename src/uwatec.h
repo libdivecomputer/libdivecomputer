b99a32d6435babff2f8dec6498252f088ea176cc
Jef Driesen <jefdriesen@users.sourceforge.net>
2008-07-08 10:06:46 +0000

    Remove all remaining pieces of the legacy api from the Uwatec code.

--- src/uwatec.h
+++ src/uwatec.h
@@ -1,15 +1,6 @@
 #ifndef UWATEC_H
 #define UWATEC_H
 
-#include "device.h"
-
-#define UWATEC_SUCCESS         0
-#define UWATEC_ERROR          -1
-#define UWATEC_ERROR_IO       -2
-#define UWATEC_ERROR_MEMORY   -3
-#define UWATEC_ERROR_PROTOCOL -4
-#define UWATEC_ERROR_TIMEOUT  -5
-
 #include "uwatec_aladin.h"
 #include "uwatec_memomouse.h"
 #include "uwatec_smart.h"
