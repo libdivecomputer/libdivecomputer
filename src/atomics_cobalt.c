5614aff3ee7dd766048bb874798d4049a6b7e48a
Jef Driesen <jefdriesen@users.sourceforge.net>
2011-02-26 09:32:15 +0100

    Add a libusb based implementation.

--- src/atomics_cobalt.c
+++ src/atomics_cobalt.c
@@ -19,8 +19,16 @@
  * MA 02110-1301 USA
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <string.h> // memcmp, memcpy
 #include <stdlib.h> // malloc, free
+
+#ifdef HAVE_LIBUSB
+#include <libusb-1.0/libusb.h>
+#endif
 
 #include "device-private.h"
 #include "atomics_cobalt.h"
@@ -28,10 +36,18 @@
 #include "utils.h"
 #include "array.h"
 
+#define VID 0x0471
+#define PID 0x0888
+#define TIMEOUT 1000
+
 #define FP_OFFSET 20
 
 typedef struct atomics_cobalt_device_t {
 	device_t base;
+#ifdef HAVE_LIBUSB
+	libusb_context *context;
+	libusb_device_handle *handle;
+#endif
 	unsigned char fingerprint[6];
 } atomics_cobalt_device_t;
 
@@ -66,6 +82,7 @@ atomics_cobalt_device_open (device_t **out)
 	if (out == NULL)
 		return DEVICE_STATUS_ERROR;
 
+#ifdef HAVE_LIBUSB
 	// Allocate memory.
 	atomics_cobalt_device_t *device = (atomics_cobalt_device_t *) malloc (sizeof (atomics_cobalt_device_t));
 	if (device == NULL) {
@@ -77,11 +94,37 @@ atomics_cobalt_device_open (device_t **out)
 	device_init (&device->base, &atomics_cobalt_device_backend);
 
 	// Set the default values.
+	device->context = NULL;
+	device->handle = NULL;
 	memset (device->fingerprint, 0, sizeof (device->fingerprint));
+
+	int rc = libusb_init (&device->context);
+	if (rc < 0) {
+		free (device);
+		return DEVICE_STATUS_IO;
+	}
+
+	device->handle = libusb_open_device_with_vid_pid (device->context, VID, PID);
+	if (device->handle == NULL) {
+		libusb_exit (device->context);
+		free (device);
+		return DEVICE_STATUS_IO;
+	}
+
+	rc = libusb_claim_interface (device->handle, 0);
+	if (rc < 0) {
+		libusb_close (device->handle);
+		libusb_exit (device->context);
+		free (device);
+		return DEVICE_STATUS_IO;
+	}
 
 	*out = (device_t*) device;
 
 	return DEVICE_STATUS_SUCCESS;
+#else
+	return DEVICE_STATUS_UNSUPPORTED;
+#endif
 }
 
 
@@ -92,6 +135,12 @@ atomics_cobalt_device_close (device_t *abstract)
 
 	if (! device_is_atomics_cobalt (abstract))
 		return DEVICE_STATUS_TYPE_MISMATCH;
+
+#ifdef HAVE_LIBUSB
+	libusb_release_interface(device->handle, 0);
+	libusb_close (device->handle);
+	libusb_exit (device->context);
+#endif
 
 	// Free memory.
 	free (device);
@@ -123,7 +172,75 @@ atomics_cobalt_device_set_fingerprint (device_t *abstract, const unsigned char d
 static device_status_t
 atomics_cobalt_read_dive (device_t *abstract, dc_buffer_t *buffer, int init)
 {
+#ifdef HAVE_LIBUSB
+	atomics_cobalt_device_t *device = (atomics_cobalt_device_t *) abstract;
+
+	if (device_is_cancelled (abstract))
+		return DEVICE_STATUS_CANCELLED;
+
+	// Erase the current contents of the buffer.
+	if (!dc_buffer_clear (buffer)) {
+		WARNING ("Insufficient buffer space available.");
+		return DEVICE_STATUS_MEMORY;
+	}
+
+	// Send the command to the dive computer.
+	uint8_t bRequest = init ? 0x09 : 0x0A;
+	int rc = libusb_control_transfer (device->handle,
+		LIBUSB_RECIPIENT_DEVICE | LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_ENDPOINT_OUT,
+		bRequest, 0, 0, NULL, 0, TIMEOUT);
+	if (rc != LIBUSB_SUCCESS)
+		return DEVICE_STATUS_IO;
+
+	unsigned int nbytes = 0;
+	while (1) {
+		// Receive the answer from the dive computer.
+		int length = 0;
+		unsigned char packet[10 * 1024] = {0};
+		rc = libusb_bulk_transfer (device->handle, 0x82,
+			packet, sizeof (packet), &length, TIMEOUT);
+		if (rc != LIBUSB_SUCCESS && rc != LIBUSB_ERROR_TIMEOUT)
+			return DEVICE_STATUS_IO;
+
+		// Append the packet to the output buffer.
+		dc_buffer_append (buffer, packet, length);
+		nbytes += length;
+
+		// If we received fewer bytes than requested, the transfer is finished.
+		if (length < sizeof (packet))
+			break;
+	}
+
+	// Check for a buffer error.
+	if (dc_buffer_get_size (buffer) != nbytes) {
+		WARNING ("Insufficient buffer space available.");
+		return DEVICE_STATUS_MEMORY;
+	}
+
+	// Check for the minimum length.
+	if (nbytes < 2) {
+		WARNING ("Data packet is too short.");
+		return DEVICE_STATUS_ERROR;
+	}
+
+#if 0
+	// Verify the checksum of the packet.
+	unsigned char *data = dc_buffer_get_data (buffer);
+	unsigned short crc = array_uint16_le (data + nbytes - 2);
+	unsigned short ccrc = checksum_add_uint16 (data, nbytes - 2, 0x0);
+	if (crc != ccrc) {
+		WARNING ("Unexpected answer CRC.");
+		return DEVICE_STATUS_PROTOCOL;
+	}
+#endif
+
+	// Remove the checksum bytes.
+	dc_buffer_slice (buffer, 0, nbytes - 2);
+
+	return DEVICE_STATUS_SUCCESS;
+#else
 	return DEVICE_STATUS_UNSUPPORTED;
+#endif
 }
 
 
