605cb37c234a28c1f973a16ce1669741bf427658
Jef Driesen <jefdriesen@users.sourceforge.net>
2012-02-24 20:36:28 +0100

    Add support for the Oceanic OC1 Pioneer.

--- src/oceanic_atom2.c
+++ src/oceanic_atom2.c
@@ -72,7 +72,8 @@ static const unsigned char oceanic_atom3_version[] = "OCEATOM3 \0\0 1024";
 static const unsigned char oceanic_vt4_version[]   = "OCEANVT4 \0\0 1024";
 static const unsigned char oceanic_vt41_version[]  = "OCEAVT41 \0\0 1024";
 static const unsigned char oceanic_geo2_version[]  = "OCEGEO20 \0\0 512K";
-static const unsigned char oceanic_oc1_version[]   = "OCWATCH R\0\0 1024";
+static const unsigned char oceanic_oc1a_version[]  = "OCWATCH R\0\0 1024";
+static const unsigned char oceanic_oc1b_version[]  = "OC1WATCH \0\0 1024";
 static const unsigned char oceanic_veo2_version[]  = "OCEVEO20 \0\0 512K";
 static const unsigned char oceanic_veo3_version[]  = "OCEVEO30 \0\0 512K";
 static const unsigned char sherwood_insight_version[] = "INSIGHT2 \0\0 512K";
@@ -329,7 +330,8 @@ oceanic_atom2_device_open (device_t **out, const char* name)
 	}
 
 	// Override the base class values.
-	if (oceanic_common_match (oceanic_oc1_version, device->version, sizeof (device->version)) ||
+	if (oceanic_common_match (oceanic_oc1a_version, device->version, sizeof (device->version)) ||
+		oceanic_common_match (oceanic_oc1b_version, device->version, sizeof (device->version)) ||
 		oceanic_common_match (oceanic_atom3_version, device->version, sizeof (device->version)) ||
 		oceanic_common_match (oceanic_vt4_version, device->version, sizeof (device->version)) ||
 		oceanic_common_match (oceanic_vt41_version, device->version, sizeof (device->version)))
