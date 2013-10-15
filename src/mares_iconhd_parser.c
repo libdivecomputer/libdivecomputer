734533e4d1cc4dbd4f4e38cc6f33d2b4cef7c5c0
Tim Wootton <tim@tee-jay.demon.co.uk>
2013-10-15 08:46:12 +0100

    Fix units for atmospheric pressure for iconhd type
    Was returning mbar, should have been bar.
    
    Signed-off-by: Tim Wootton <tim@tee-jay.demon.co.uk>

--- src/mares_iconhd_parser.c
+++ src/mares_iconhd_parser.c
@@ -187,7 +187,8 @@ mares_iconhd_parser_get_field (dc_parser_t *abstract, dc_field_type_t type, unsi
 			gasmix->nitrogen = 1.0 - gasmix->oxygen - gasmix->helium;
 			break;
 		case DC_FIELD_ATMOSPHERIC:
-			*((double *) value) = array_uint16_le (p + 0x26) / 8;
+			// Pressure (1/8 millibar)
+			*((double *) value) = array_uint16_le (p + 0x26) / 8000.0;
 			break;
 		default:
 			return DC_STATUS_UNSUPPORTED;
