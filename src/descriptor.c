a67e21dc2f42f745aeb291a67bec639cf61e443b
Jef Driesen <jefdriesen@users.sourceforge.net>
2015-03-18 06:29:48 +0100

    Add support for two new OSTC3 variants.
    The new OSTC3 (model 0x1A) is identical to the existing OSTC3, except
    that the usb interface has been replaced with a bluetooth interface.
    
    The new OSTC2 (model 0x1A) will retain the look and feel of the OSTC 2C
    (e.g. plastic housing, piezo buttons and rechargable battery), but under
    the hood it will use the same firmware as the OSTC3. It will also have a
    bluetooth interface.

--- src/descriptor.c
+++ src/descriptor.c
@@ -210,7 +210,9 @@ static const dc_descriptor_t g_descriptors[] = {
 	{"Heinrichs Weikamp", "OSTC 2N",  DC_FAMILY_HW_OSTC, 2},
 	{"Heinrichs Weikamp", "OSTC 2C",  DC_FAMILY_HW_OSTC, 3},
 	{"Heinrichs Weikamp", "Frog",     DC_FAMILY_HW_FROG, 0},
+	{"Heinrichs Weikamp", "OSTC 2",     DC_FAMILY_HW_OSTC3, 0x11},
 	{"Heinrichs Weikamp", "OSTC 3",     DC_FAMILY_HW_OSTC3, 0x0A},
+	{"Heinrichs Weikamp", "OSTC 3",     DC_FAMILY_HW_OSTC3, 0x1A},
 	{"Heinrichs Weikamp", "OSTC cR",    DC_FAMILY_HW_OSTC3, 0x05},
 	{"Heinrichs Weikamp", "OSTC Sport", DC_FAMILY_HW_OSTC3, 0x12},
 	/* Cressi Edy */
