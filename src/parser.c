f107d7c0d8242e3f07b0b8ed5c9c041cc733c8f8
Linus Torvalds <torvalds@linux-foundation.org>
2017-06-08 22:16:14 -0700

    Add initial Scubapro G2 frontend
    The back-end parser seems to be the same as for the Uwatec Smart (aka
    Galileo Sol).  At least that's the assumption right now.
    
    The downloader just uses USB HID (very similar to EON Steel) rather than
    the horrible IrDA thing.
    
    There's also eventually a BLE thing, but that's for the future.
    
    This is an unholy mixture of the Uwatec Smart downloader logic and the
    EON Steel usbhid transfer code.  The back-end is pure Uwatec Smart
    (model 0x11, same as Galileo Sol).
    
    I'm not at all sure this gets everything right, but it downloads
    *something*.
    
    [Jef Driesen: Renamed the backend to uwatec, and made some smaller
                  cosmetic changes to match the existing coding style.]
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- src/parser.c
+++ src/parser.c
@@ -100,6 +100,9 @@ dc_parser_new_internal (dc_parser_t **out, dc_context_t *context, dc_family_t fa
 	case DC_FAMILY_UWATEC_MERIDIAN:
 		rc = uwatec_smart_parser_create (&parser, context, model, devtime, systime);
 		break;
+	case DC_FAMILY_UWATEC_G2:
+		rc = uwatec_smart_parser_create (&parser, context, 0x11, devtime, systime);
+		break;
 	case DC_FAMILY_REEFNET_SENSUS:
 		rc = reefnet_sensus_parser_create (&parser, context, devtime, systime);
 		break;
