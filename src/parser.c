0a5623a949eef1c12fb1ce39e4941b31f0c29eb1
Jef Driesen <jefdriesen@users.sourceforge.net>
2017-06-23 06:55:14 +0200

    Use the correct model number
    The model number reported by the Scubapro G2 is 0x32.

--- src/parser.c
+++ src/parser.c
@@ -98,10 +98,8 @@ dc_parser_new_internal (dc_parser_t **out, dc_context_t *context, dc_family_t fa
 		break;
 	case DC_FAMILY_UWATEC_SMART:
 	case DC_FAMILY_UWATEC_MERIDIAN:
-		rc = uwatec_smart_parser_create (&parser, context, model, devtime, systime);
-		break;
 	case DC_FAMILY_UWATEC_G2:
-		rc = uwatec_smart_parser_create (&parser, context, 0x11, devtime, systime);
+		rc = uwatec_smart_parser_create (&parser, context, model, devtime, systime);
 		break;
 	case DC_FAMILY_REEFNET_SENSUS:
 		rc = reefnet_sensus_parser_create (&parser, context, devtime, systime);
