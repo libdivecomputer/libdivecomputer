d327aea6ff41c748dfb9e590ca32ed1bf433040d
Jef Driesen <jef@libdivecomputer.org>
2023-02-10 21:06:51 +0100

    Add a reflected variant of the 16-bit CRC-CCITT
    The reflected variant of a CRC does reverse the bits of each input byte
    and requires a different lookup table for an efficient implementation.

--- src/checksum.h
+++ src/checksum.h
@@ -41,6 +41,9 @@ checksum_xor_uint8 (const unsigned char data[], unsigned int size, unsigned char
 unsigned short
 checksum_crc16_ccitt (const unsigned char data[], unsigned int size, unsigned short init, unsigned short xorout);
 
+unsigned short
+checksum_crc16r_ccitt (const unsigned char data[], unsigned int size, unsigned short init, unsigned short xorout);
+
 unsigned int
 checksum_crc32 (const unsigned char data[], unsigned int size);
 
