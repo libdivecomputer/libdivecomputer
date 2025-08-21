765d6dab3ff8a404d8f7092801f31dfc5aca7c4e
tim661811 <stantim@hotmail.com>
2025-08-21 13:49:42 +0200

    Add support for limiting the number of dives to download in dctool

--- examples/dctool_download.c
+++ examples/dctool_download.c
@@ -53,6 +53,7 @@ typedef struct dive_data_t {
 	dc_buffer_t **fingerprint;
 	unsigned int number;
 	dctool_output_t *output;
+	unsigned int count;
 } dive_data_t;
 
 static int
@@ -92,6 +93,10 @@ dive_cb (const unsigned char *data, unsigned int size, const unsigned char *fing
 	if (rc != DC_STATUS_SUCCESS) {
 		ERROR ("Error parsing the dive data.");
 		goto cleanup;
+	}
+	
+	if (divedata->count > 0 && divedata->number >= divedata->count) {
+		return 0;
 	}
 
 cleanup:
@@ -146,7 +151,7 @@ event_cb (dc_device_t *device, dc_event_type_t event, const void *data, void *us
 }
 
 static dc_status_t
-download (dc_context_t *context, dc_descriptor_t *descriptor, dc_transport_t transport, const char *devname, const char *cachedir, dc_buffer_t *fingerprint, dctool_output_t *output)
+download (dc_context_t *context, dc_descriptor_t *descriptor, dc_transport_t transport, const char *devname, const char *cachedir, dc_buffer_t *fingerprint, dctool_output_t *output, unsigned int count)
 {
 	dc_status_t rc = DC_STATUS_SUCCESS;
 	dc_iostream_t *iostream = NULL;
@@ -214,6 +219,7 @@ download (dc_context_t *context, dc_descriptor_t *descriptor, dc_transport_t tra
 	divedata.fingerprint = &ofingerprint;
 	divedata.number = 0;
 	divedata.output = output;
+	divedata.count = count;
 
 	// Download the dives.
 	message ("Downloading the dives.\n");
@@ -231,7 +237,7 @@ download (dc_context_t *context, dc_descriptor_t *descriptor, dc_transport_t tra
 		// Generate the fingerprint filename.
 		family = dc_device_get_type (device);
 		snprintf (filename, sizeof (filename), "%s/%s-%08X.bin",
-			cachedir, dctool_family_name (family), eventdata.devinfo.serial);
+					cachedir, dctool_family_name (family), eventdata.devinfo.serial);
 
 		// Write the fingerprint file.
 		dctool_file_write (filename, ofingerprint);
@@ -260,10 +266,11 @@ dctool_download_run (int argc, char *argv[], dc_context_t *context, dc_descripto
 	const char *filename = NULL;
 	const char *cachedir = NULL;
 	const char *format = "xml";
+	unsigned int count = 0;
 
 	// Parse the command-line options.
 	int opt = 0;
-	const char *optstring = "ht:o:p:c:f:u:";
+	const char *optstring = "ht:o:p:c:f:u:n:";
 #ifdef HAVE_GETOPT_LONG
 	struct option options[] = {
 		{"help",        no_argument,       0, 'h'},
@@ -273,6 +280,7 @@ dctool_download_run (int argc, char *argv[], dc_context_t *context, dc_descripto
 		{"cache",       required_argument, 0, 'c'},
 		{"format",      required_argument, 0, 'f'},
 		{"units",       required_argument, 0, 'u'},
+		{"count",       required_argument, 0, 'n'},
 		{0,             0,                 0,  0 }
 	};
 	while ((opt = getopt_long (argc, argv, optstring, options, NULL)) != -1) {
@@ -303,6 +311,9 @@ dctool_download_run (int argc, char *argv[], dc_context_t *context, dc_descripto
 				units = DCTOOL_UNITS_METRIC;
 			if (strcmp (optarg, "imperial") == 0)
 				units = DCTOOL_UNITS_IMPERIAL;
+			break;
+		case 'n':
+			count = atoi (optarg);
 			break;
 		default:
 			return EXIT_FAILURE;
@@ -345,7 +356,7 @@ dctool_download_run (int argc, char *argv[], dc_context_t *context, dc_descripto
 	}
 
 	// Download the dives.
-	status = download (context, descriptor, transport, argv[0], cachedir, fingerprint, output);
+	status = download (context, descriptor, transport, argv[0], cachedir, fingerprint, output, count);
 	if (status != DC_STATUS_SUCCESS) {
 		message ("ERROR: %s\n", dctool_errmsg (status));
 		exitcode = EXIT_FAILURE;
@@ -375,6 +386,7 @@ const dctool_command_t dctool_download = {
 	"   -c, --cache <directory>    Cache directory\n"
 	"   -f, --format <format>      Output format\n"
 	"   -u, --units <units>        Set units (metric or imperial)\n"
+	"   -n, --count <number>       Number of dives to download\n"
 #else
 	"   -h                 Show help message\n"
 	"   -t <transport>     Transport type\n"
@@ -383,6 +395,7 @@ const dctool_command_t dctool_download = {
 	"   -c <directory>     Cache directory\n"
 	"   -f <format>        Output format\n"
 	"   -u <units>         Set units (metric or imperial)\n"
+	"   -n <count>         Number of dives to download\n"
 #endif
 	"\n"
 	"Supported output formats:\n"
